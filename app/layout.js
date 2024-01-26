5d704c15aada617cdabfa9ba35a24ce50aa4a52d
AESiR <cyborgkiller1008@gmail.com>
2024-01-26 13:50:17 +0530

    heheh

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,5 @@
 import localFont from 'next/font/local'
 import './globals.css'
-import icon from '@/public/favicon.ico'
 
 const jostFont = localFont({ src: './Jost-VariableFont_wght.ttf', variable: "--font-jost", display: "swap" })
 
@@ -14,7 +13,6 @@ export default function RootLayout({ children }) {
   return (
     
     <html lang="en">
-      <link rel="icon" href={icon} sizes="32x32" type="image/x-icon" />
       <body className={jostFont.className}>
       <main className='w-full max-h-screen '>
         {children}
