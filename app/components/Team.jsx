22ddc2cc526df8e03037a16d757bcce64b7c5b1d
AESiR <cyborgkiller1008@gmail.com>
2024-01-30 20:24:45 +0530

    Update Team component to include a Facilitator card

--- app/components/Team.jsx
+++ app/components/Team.jsx
@@ -5,6 +5,14 @@ import Image3 from "@/public/static/images/organizer/3.jpg";
 import Image4 from "@/public/static/images/organizer/himank.jpeg";
 import Image5 from "@/public/static/images/organizer/4.jpg";
 
+
+const faciData = [
+  {
+    name: "Himank Varshney",
+    designation: "Facilitator",
+    image: Image4,
+  }
+]
 const TeamData = [
   {
     name: "Rohit Shukla",
@@ -30,20 +38,21 @@ const TeamData = [
     designation: "Organizer",
     image: Image5,
     number: "+91 6353935 670",
-  },
-  {
-    name: "Himank Varshney",
-    designation: "Facilitator",
-    image: Image4,
   }
 ];
 
 export default function Team() {
   return (
     <div id="team" className="w-screen min-h-screen flex flex-col z-10 flex-wrap justify-center py-10 items-center gap-24 max-md:min-h-screen">
-      <h1 className="text-5xl font-semibold max-md:text-3xl">MEET OUR ORGANIZERS</h1>
+      <h1 className="text-5xl font-semibold max-md:text-3xl">MEET OUR TEAM</h1>
       <div className="w-screen justify-evenly flex flex-wrap gap-x-36 gap-y-10 items-center max-md:flex-col max-md:gap-6">
         {TeamData.map((item, index) => {
+          return <CardGenerator props={item} key={index} />;
+        })}
+      </div>
+      <h1 className="text-4xl font-semibold max-md:text-2xl">Facilitator</h1>
+      <div className="w-screen justify-center flex max-md:flex-col max-md:gap-6">
+        {faciData.map((item, index) => {
           return <CardGenerator props={item} key={index} />;
         })}
       </div>
