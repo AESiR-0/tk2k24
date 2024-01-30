334964d54279e1dfb4a42921917f4d7f21e2747a
AESiR <cyborgkiller1008@gmail.com>
2024-01-30 20:33:48 +0530

    Update HeroImage component styling and add event date

--- app/components/HeroImage.jsx
+++ app/components/HeroImage.jsx
@@ -5,8 +5,9 @@ import Link from "next/link";
 
 export default function HeroImage() {
   return (
-    <div className="flex flex-col w-screen h-screen justify-center  items-center">
+    <div className="flex flex-col w-screen h-screen justify-center   items-center">
       <Image src={HImage} width={720} height={720} alt="tk.logo" />
+      <h1 className="text-3xl font-jost max-md:mt-0 opacity-85 -mt-20 mb-20">15th & 16th February</h1>
       <Link href="https://tally.so/r/mOG9xM">
         <Button className="scale-125"> Register Now </Button>
       </Link>
