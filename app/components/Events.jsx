ecf5e201fcc8e1813ae2e1132b0357d8c5f07811
AESiR-0 <90002355+AESiR-0@users.noreply.github.com>
2024-01-31 16:49:48 +0530

    Update Events.jsx

--- app/components/Events.jsx
+++ app/components/Events.jsx
@@ -14,7 +14,7 @@ import valo from "@/public/static/images/events/games/valorant.png";
 import EventCard from "@/components/EventCard";
 import bgmi from "@/public/static/images/events/games/Bgmi.png";
 
-const ruleBook = "https://drive.google.com/file/d/1UBiZrFbJtg8SmoMUALfeB03rA4tgH9qx/view?usp=drive_link"
+const ruleBook = "https://drive.google.com/file/d/1d7l97cUOF4aCa3ir3lQRvtCYZnLiyp6n/view?usp=drive_link"
 const techEvents = [
   { title: "Code Complete", src: codeComplete },
   { title: "Code Swap", src: codeSwap },
