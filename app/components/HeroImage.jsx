4a41eb3a6227fcb3edd7b296cc3bca48850c74d0
AESiR <cyborgkiller1008@gmail.com>
2024-02-02 19:29:12 +0530

    Update HeroImage component with event details and location

--- app/components/HeroImage.jsx
+++ app/components/HeroImage.jsx
@@ -7,7 +7,13 @@ export default function HeroImage() {
   return (
     <div className="flex flex-col w-screen h-screen justify-center   items-center">
       <Image src={HImage} width={720} height={720} alt="tk.logo" />
-      <h1 className="text-3xl font-jost max-md:mt-0 opacity-85 -mt-20 mb-20">15th & 16th February</h1>
+      <h1 className="text-3xl font-jost max-md:mt-0 opacity-85 text-center max-w-lg -mt-20 mb-20">
+        15th & 16th February <br />
+        <br />
+        <Link href={"https://maps.app.goo.gl/7a5fiJZK7EuC43ae9"}>
+          Department of Computer Science, Gujarat University
+        </Link>
+      </h1>
       <Link href="https://tally.so/r/mOG9xM">
         <Button className="scale-125"> Register Now </Button>
       </Link>
